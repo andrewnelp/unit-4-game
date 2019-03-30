d53384b7316af739b29a571b76866b4476178d87
Andrey Nelepov <nelepow@gmail.com>
2019-03-30 13:58:42 -0400

    still using the same last array digit

--- Assets/JS/game.js
+++ Assets/JS/game.js
@@ -11,15 +11,17 @@ var three;
 var four;
 var res = $('#result');
 var audioElement = document.createElement("audio");
-      var arr = [getRandom(1, 12), getRandom(1, 12), getRandom(1, 12), getRandom(1, 12)];
-      console.log(arr)
-
+  var arr = [];
+  var iCrystal = $("img");
+  
 // create a startGame function
 function startGame() {
 one = getRandom(1, 12);
 two = getRandom(1, 12);
 three = getRandom(1, 12);
 four = getRandom(1, 12);
+arr = [one, two, three, four];
+  console.log(arr);
 mNumber = getRandom(19, 120);
 $('#mNum').text(mNumber);
 $('#wins').text(wins);
@@ -39,79 +41,49 @@ function getRandom(min, max) {
 }
 
 // create on click events for one, two...and find totalScore sum
-// Main Game
-// for (var i = 0; i < arr.length; i++) {
-//   var iCrystal = $("img");
-//     //set data attr to each image
-//   iCrystal.attr('data-crystalvalue', arr[i]);
-// }
+// Main Game - the problem is that it data referes to the last array i and array never updates
+function dataAttr(){
+  for (var i = 0; i < arr.length; i++) {
+    //set data attr to each image
+    iCrystal.attr('data-crystalvalue', arr[i]);
+  }
+  return iCrystal;
+}
+  dataAttr();
 
-//   iCrystal.on('click', function() {
-//     // Determining the crystal's value requires us to extract the value from the data attribute.
-//     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-//     // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-//     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
-//     var crystalValue = ($(this).attr("data-crystalvalue"));
-//     crystalValue = parseInt(crystalValue);
-//     // $(this).animate({ width: '+=5px', opacity: '0.7' }, 300);
-//     // $(this).animate({ width: '-=5px', opacity: '1.0' }, 300);
-//     totalScore += crystalValue;
-//     $('#totScore').text(totalScore);
-//     logic()
-//   })
+
+  iCrystal.on('click', function() {
+    // Determining the crystal's value requires us to extract the value from the data attribute.
+    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+    
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    var crystalValueParse = parseInt(crystalValue);
+    $(this).animate({ width: '+=2px', opacity: '0.5' }, 200);
+    $(this).animate({ width: '-=2px', opacity: '1.0' }, 200);
+    totalScore += crystalValueParse;
+    $('#totScore').text(totalScore);
+    logic();
+  })
 
   // set logic for your Total Score id='totScore'
   function logic(){
     if (totalScore == mNumber) {
       wins = wins + 1;
-      res.text('You Win!').css('color', 'white').animate({ opacity: "0.1" }, 1000).animate({ opacity: "1" }, 1000);
+      res.text('You Win!').css('color', 'white').animate({ opacity: "0.01" }, 1000).animate({ opacity: "1", fontSize: "2rem"}, 1000);
       audioElement.setAttribute("src", "Assets/Sound/splits.mp3")
       audioElement.play();
       startGame();
     }
     else if (totalScore > mNumber) {
       losses++;
-      res.text('You Lose!').css('color', 'red').animate({ opacity: "0.1" }, 1000).animate({ opacity: "1" }, 1000);
+      res.text('You Lose!').css('color', 'red').animate({ opacity: "0.01" }, 1000).animate({ opacity: "1", fontSize: "2rem"}, 1000);
       audioElement.setAttribute("src", "Assets/Sound/suspension.mp3")
       audioElement.play();
       startGame();
     }
   }
-
-  $('img').eq(0).on('click dblclick', function () {
-    $(this).animate({ width: '+=2px', opacity: '0.7' }, 300);
-    $(this).animate({ width: '-=2px', opacity: '1.0' }, 300);
-    logic()
-    totalScore += one;
-    $('#totScore').text(totalScore);
-  })
-  
-
-  $('img').eq(1).on('click dblclick', function(){
-    $(this).animate({ width: '+=2px', opacity: '0.7' }, 300);
-    $(this).animate({ width: '-=2px', opacity: '1.0' }, 300);
-    logic()
-  totalScore += two;
-  $('#totScore').text(totalScore);
-})
-
-  $('img').eq(2).on('click dblclick', function(){
-    $(this).animate({ width: '+=2px', opacity: '0.7' }, 200);
-    $(this).animate({ width: '-=2px', opacity: '1.0' }, 200);
-    logic()
-  totalScore += three;
-  $('#totScore').text(totalScore);
-})
-
-  $('img').eq(3).on('click dblclick', function(){
-    $(this).animate({ width: '+=2px', opacity: '0.7' }, 300);
-    $(this).animate({ width: '-=2px', opacity: '1.0' }, 300);
-    logic()
-  totalScore += four;
-  $('#totScore').text(totalScore);
-})
-
-
 
 });
