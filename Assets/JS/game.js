df19f49ce92321379efc8f74e786ca7ef984baee
Andrey Nelepov <nelepow@gmail.com>
2019-03-31 14:30:11 -0400

    all is working now

--- Assets/JS/game.js
+++ Assets/JS/game.js
@@ -11,8 +11,7 @@ var three;
 var four;
 var res = $('#result');
 var audioElement = document.createElement("audio");
-  var arr = [];
-  var iCrystal = $("img");
+var iCrystal = $('img');
   
 // create a startGame function
 function startGame() {
@@ -21,49 +20,63 @@ two = getRandom(1, 12);
 three = getRandom(1, 12);
 four = getRandom(1, 12);
 arr = [one, two, three, four];
-  console.log(arr);
+console.log(arr);
 mNumber = getRandom(19, 120);
-$('#mNum').text(mNumber);
-$('#wins').text(wins);
-$('#losses').text(losses);
-// document. querySelector('#wins').dataset.winScore = wins;
-// document.querySelector('#losses').dataset.lossesScore = losses;
-totalScore = 0
-$('#totScore').text(totalScore);
+  $('#mNum').text(mNumber);
+  $('#wins').text(wins);
+  $('#losses').text(losses);
+  totalScore = 0
+  $('#totScore').text(totalScore);
 }
 
 startGame();
 
   // create Math random function
-
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 // create on click events for one, two...and find totalScore sum
 // Main Game - the problem is that it data referes to the last array i and array never updates
-function dataAttr(){
-  for (var i = 0; i < arr.length; i++) {
-    //set data attr to each image
-    iCrystal.attr('data-crystalvalue', arr[i]);
-  }
-  return iCrystal;
-}
-  dataAttr();
 
 
-
-  iCrystal.on('click', function() {
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+  $('#img1').on('click', function() {
+    var crystalValue = arr[0];
     
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    var crystalValueParse = parseInt(crystalValue);
+    // crystalValue = parseInt(crystalValue);
     $(this).animate({ width: '+=2px', opacity: '0.5' }, 200);
     $(this).animate({ width: '-=2px', opacity: '1.0' }, 200);
-    totalScore += crystalValueParse;
+    totalScore += crystalValue;
+    $('#totScore').text(totalScore);
+    logic();
+  })
+  $('#img2').on('click', function() {
+    var crystalValue = arr[1];
+    
+    // crystalValue = parseInt(crystalValue);
+    $(this).animate({ width: '+=2px', opacity: '0.5' }, 200);
+    $(this).animate({ width: '-=2px', opacity: '1.0' }, 200);
+    totalScore += crystalValue;
+    $('#totScore').text(totalScore);
+    logic();
+  })
+  $('#img3').on('click', function() {
+    var crystalValue = arr[2];
+    
+    // crystalValue = parseInt(crystalValue);
+    $(this).animate({ width: '+=2px', opacity: '0.5' }, 200);
+    $(this).animate({ width: '-=2px', opacity: '1.0' }, 200);
+    totalScore += crystalValue;
+    $('#totScore').text(totalScore);
+    logic();
+  })
+  $('#img4').on('click', function() {
+    var crystalValue = arr[3];
+    
+    // crystalValue = parseInt(crystalValue);
+    $(this).animate({ width: '+=2px', opacity: '0.5' }, 200);
+    $(this).animate({ width: '-=2px', opacity: '1.0' }, 200);
+    totalScore += crystalValue;
     $('#totScore').text(totalScore);
     logic();
   })
